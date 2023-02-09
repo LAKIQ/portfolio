fbe4259daa0b5e1f845588780a27c04733e46da2
LAKIQ <104293375+LAKIQ@users.noreply.github.com>
2023-02-10 00:10:21 +0700

    Update MainItem.jsx

--- src/components/MainItem.jsx
+++ src/components/MainItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MainItem = () => {
+const MainItem = ({img}) => {
   return (
     <div >
     <img  src={img} alt="Me 1" className='w-full h-screen object-cover ' />
