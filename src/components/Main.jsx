db5fb11984b9e388990b6d2aba721d20534b3d3b
LAKIQ <104293375+LAKIQ@users.noreply.github.com>
2023-02-09 23:55:07 +0700

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import {FaFacebookF,FaGithub,FaLinkedinIn} from 'react-icons/fa'
 const Main = () => {
   return (
     <div id='main'>
-      <img className='w-full h-screen object-cover ' src="../assets/me1.jpg'" alt="me1" />
+      <img className='w-full h-screen object-cover ' src="../assets/me1.jpg" alt="me1" />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/30'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
                 <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>MY NAME IS PAT</h1>
